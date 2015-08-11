eca5f5730cd9b1d12aec02cf8b8019d83a038b1e
Tara Mahoney <tara.mahoney@me.com>
2015-08-11 19:21:21 -0400

    configure AWS for image uploading

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,5 @@ require "paperclip"
 
 Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.1-Q16'
 Paperclip.options[:swallow_stderr] = false
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
