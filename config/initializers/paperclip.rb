63cc12ab651c05fc3366c3225d5d250a9fd7f363
Tara Mahoney <tara.mahoney@me.com>
2015-08-12 15:15:03 -0400

    changing region

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,4 +4,3 @@ Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.1-Q16'
 Paperclip.options[:swallow_stderr] = false
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
