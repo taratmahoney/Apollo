420aec8085b58987ae8dd7195e295df58f990a90
Tara Mahoney <tara.mahoney@me.com>
2015-08-12 11:08:43 -0400

    add self methods for attachment to posts

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '2.2.1'
 gem 'bourbon'
 gem 'neat'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ db/migrate/20150812150709_add_image_to_posts.rb
@@ -0,0 +1,11 @@
+class AddImageToPosts < ActiveRecord::Migration
+  def change
+      def self.up
+        add_attachment :posts, :image
+      end
+
+  def self.down
+    remove_attachment :posts, :image
+  end
+  end
+end
