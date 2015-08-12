22a653dfea848c3882f9cb15a0b45805e486204f
Tara Mahoney <tara.mahoney@me.com>
2015-08-12 11:14:55 -0400

    add file field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811144014) do
+ActiveRecord::Schema.define(version: 20150812150709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
